e4059335f296e432a04c67a6347c5dd527447328
Yue Zhu <zhuyuejoey@gmail.com>
2024-11-15 22:09:55 +0800

    bug fixed

--- src/config/basicConfig.js
+++ src/config/basicConfig.js
@@ -4,7 +4,7 @@ const config = {
         'kreadoai.com',
         'pages.kreadoai.com',
         'websitelm.com',
-        'page.websitelm.com',
+        'pages.websitelm.com',
     ],
     token: 'DV4BYXlD5kHtVX5jU86UDB7Z3s0C1JLuD0zviO72ECqZ6FcHElKRVOsAOt9spuDu'
 };
