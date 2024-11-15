125cb68a319ee1c79f1d6a46ac8ca0704c21b0ed
Yue Zhu <zhuyuejoey@gmail.com>
2024-11-15 22:23:55 +0800

    bug fixed

--- src/config/basicConfig.js
+++ src/config/basicConfig.js
@@ -1,10 +1,12 @@
 // customerDomains.js
 const config = {
     domains: [
-        'kreadoai.com',
-        'pages.kreadoai.com',
+        // 'kreadoai.com',
+        // 'pages.kreadoai.com',
         'websitelm.com',
         'pages.websitelm.com',
+        'pages.zhuyue.tech',
+        'zhuyue.tech'
     ],
     token: 'DV4BYXlD5kHtVX5jU86UDB7Z3s0C1JLuD0zviO72ECqZ6FcHElKRVOsAOt9spuDu'
 };
