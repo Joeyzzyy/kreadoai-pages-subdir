9fb5cc0858c1d8af99a22d22186acd2c98ee694a
Yue Zhu <zhuyuejoey@gmail.com>
2024-11-17 01:45:16 +0800

    bug fixd

--- next.config.js
+++ next.config.js
@@ -1,26 +1,54 @@
 /** @type {import('next').NextConfig} */
 
+const DOMAIN = process.env.DOMAIN;
+
+const isLocal = process.env.NODE_ENV === 'development';
+
 const nextConfig = {
   images: {
-    domains: [process.env.DOMAIN],
+    domains: isLocal ? [] : [DOMAIN],
     unoptimized: true,
   },
   async rewrites() {
+    if (isLocal) {
+      // 本地开发环境，禁用 rewrites
+      return [];
+    }
     return [
       {
         source: '/articles/:slug*',
-        destination: `https://${process.env.DOMAIN}/articles/:slug*`,
+        destination: `https://${DOMAIN}/articles/:slug*`,
         basePath: false,
-        // has: [
-        //   {
-        //     type: 'header',
-        //     key: 'x-rewrite',
-        //     missing: true
-        //   }
-        // ]
-      }
-    ]
-  }
-}
+      },
+    ];
+  },
+  async redirects() {
+    if (isLocal) {
+      // 本地开发环境，禁用 redirects
+      return [];
+    }
+    return [
+      {
+        source: '/:path((?!articles/).*)',
+        has: [
+          {
+            type: 'header',
+            key: 'x-forwarded-host',
+            value: DOMAIN,
+          },
+          {
+            type: 'query',
+            key: 'redirected',
+            value: '1',
+            negate: true,
+          },
+        ],
+        destination: `https://${DOMAIN}/:path*?redirected=1`,
+        permanent: false,
+        basePath: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
